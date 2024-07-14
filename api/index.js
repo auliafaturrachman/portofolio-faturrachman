7f2441dead84c5200b3568e4035f44f0bf2a7e46
Aulia Faturrachman <30308265+auliafaturrachman@users.noreply.github.com>
2024-07-14 11:01:44 +0700

    edit: api folder created

--- api/index.js
+++ api/index.js
@@ -8,3 +8,5 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
