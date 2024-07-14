02229d7db24f7c4c98eff53ed74419a583edcb51
Aulia Faturrachman <30308265+auliafaturrachman@users.noreply.github.com>
2024-07-14 10:32:24 +0700

    add: vercel config

--- api/index.js
+++ api/index.js
@@ -1,12 +1,10 @@
 const path = require("path");
-const http = require("http");
 const express = require("express");
 const app = express();
-const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, "client")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
